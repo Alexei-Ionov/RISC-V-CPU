41520a4fe8a55752f8062e176ddbad10310d4e0e
Alexei Ionov <alexei.ionov@berkeley.edu>
2023-04-04 01:28:25 -0700

    testing load half

--- tests/integration-custom/in/immediate_test.s
+++ tests/integration-custom/in/immediate_test.s
@@ -4,14 +4,6 @@ addi s1, s1, 1864
 sw   s1, 80(s0)
 sw   s1, -20(s0)
 lw   t0, 80(s0)
-
-lb   t0, 80(s0)
-lb   t1, 81(s0)
-lb   t2, 82(s0)
-lb   t0, 83(s0)
-lw   t0, -204(s0)
-
-lb   t0, -204(s0)
-lb   t1, -203(s0)
-lb   t2, -202(s0)
-lb   t0, -201(s0)
+lh   t1, 80(s0)
+lh t2, 80(s0)
+lh ra -20(s0)
